d60c9236073bf6488cd7e59e5014bb283422f869
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-03-10 23:37:03 -0500

    some changes

--- src/main/java/net/anvian/perfume/PerfumeMod.java
+++ src/main/java/net/anvian/perfume/PerfumeMod.java
@@ -8,15 +8,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PerfumeMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("perfume");
 	public static final String MOD_ID = "perfume";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
-		ModStatusEffects.registerEffects();
-
 		ModItems.registerModItems();
+
+		ModStatusEffects.registerEffects();
 
 		ModSounds.registerModSound();
 
