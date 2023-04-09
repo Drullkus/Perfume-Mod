6d4e718ae7ffabe7e9baf4d88c552eb73010cb55
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-04-09 16:39:08 -0500

    dataGen was move

--- src/main/java/net/anvian/perfume/dataGen/ExampleModDataGenerator.java
+++ src/main/java/net/anvian/perfume/dataGen/ExampleModDataGenerator.java
@@ -1,4 +1,4 @@
-package net.anvian.perfume;
+package net.anvian.perfume.dataGen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
