c8a99aa447f75eb751563f8a7286e679d4de4342
anviaan <andre.carbajal.vargas@gmail.com>
2023-06-02 19:51:22 -0500

    ModStatusEffects was refactored

--- src/main/java/net/anvian/perfume/statuseffect/ModStatusEffects.java
+++ src/main/java/net/anvian/perfume/statuseffect/ModStatusEffects.java
@@ -7,9 +7,13 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModStatusEffects {
-    public static final StatusEffect CarrotEffect = new CarrotEffect();
+    public static final StatusEffect CarrotEffect = registerStatusEffects("carrot_effect", new CarrotEffect());
+
+    public static StatusEffect registerStatusEffects(String name, StatusEffect statusEffect){
+        return Registry.register(Registry.STATUS_EFFECT, new Identifier(PerfumeMod.MOD_ID, name), statusEffect);
+    }
 
     public static void registerEffects(){
-        Registry.register(Registry.STATUS_EFFECT, new Identifier(PerfumeMod.MOD_ID, "carrot_effect"), CarrotEffect);
+        PerfumeMod.LOGGER.debug("Registering Mod StatusEffects for " + PerfumeMod.MOD_ID);
     }
 }
