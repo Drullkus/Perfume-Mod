bda80a7d3c489b3ba8e93985baa0900269a7a2af
anviaan <andre.carbajal.vargas@gmail.com>
2023-06-18 16:34:16 -0500

    Item Group icon was changed

--- src/main/java/net/anvian/perfume/items/ModItemGroup.java
+++ src/main/java/net/anvian/perfume/items/ModItemGroup.java
@@ -4,12 +4,11 @@ import net.anvian.perfume.PerfumeMod;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
     public static final ItemGroup PERFUME_GROUP = FabricItemGroupBuilder.build(
             new Identifier(PerfumeMod.MOD_ID, "perfume_group"),
-            () -> new ItemStack(Items.GLASS_BOTTLE)
+            () -> new ItemStack(ModItems.WATER_PERFUME_BOTTLE)
     );
 }
