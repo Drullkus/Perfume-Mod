c7d5c7b6a76516b9e18e8f0d3d38cb3611e8bc2d
anviaan <andre.carbajal.vargas@gmail.com>
2023-07-26 11:38:03 -0500

    fix deprecated

--- src/main/java/net/anvian/perfume/PerfumeModClient.java
+++ src/main/java/net/anvian/perfume/PerfumeModClient.java
@@ -6,7 +6,6 @@ import net.anvian.perfume.screen.PerfumeMachineScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 
 @Environment(EnvType.CLIENT)
