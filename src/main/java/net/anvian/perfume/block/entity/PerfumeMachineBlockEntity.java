e7749945c85da0b5b57c5dc067def955a9667c17
anviaan <andre.carbajal.vargas@gmail.com>
2023-11-12 22:20:13 -0500

    A sound was added when the machine finished making the perfume.

--- src/main/java/net/anvian/perfume/block/entity/PerfumeMachineBlockEntity.java
+++ src/main/java/net/anvian/perfume/block/entity/PerfumeMachineBlockEntity.java
@@ -17,6 +17,7 @@ import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.text.Text;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
@@ -128,6 +129,7 @@ public class PerfumeMachineBlockEntity extends BlockEntity implements ExtendedSc
                     markDirty(world, pos, state);
 
                     if(hasCraftingFinished()) {
+                        world.playSound(null, pos, ModSounds.PIP_SOUND, SoundCategory.BLOCKS, 1.0f, 1.0f);
                         this.craftItem();
                         this.resetProgress();
                     }
--- src/main/java/net/anvian/perfume/sound/ModSounds.java
+++ src/main/java/net/anvian/perfume/sound/ModSounds.java
@@ -8,6 +8,7 @@ import net.minecraft.util.Identifier;
 
 public class ModSounds {
     public static SoundEvent PERFUME_SOUND = registerSoundEvent("perfume_sound");
+    public static SoundEvent PIP_SOUND = registerSoundEvent("pip_sound");
 
     private static SoundEvent registerSoundEvent(String name) {
         Identifier id = new Identifier(PerfumeMod.MOD_ID, name);
